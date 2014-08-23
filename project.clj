db68ead1366bc439890d0f5de35720bbf20c5689
Travis Thieman <travis.thieman@gmail.com>
2014-08-22 21:09:03 -0400

    Move private config out of the project

--- project.clj
+++ project.clj
@@ -4,6 +4,4 @@
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/core.async "0.1.338.0-5c5012-alpha"]]
-  :deploy-repositories [["clojars" {:url "https://clojars.org/repo"
-                                    :sign-releases false}]])
+                 [org.clojure/core.async "0.1.338.0-5c5012-alpha"]])
